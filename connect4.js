1f16518d8eafacc5222d90c3b22c076cf84e7651
Jose De la Cruz <jdelacruzjose@gmail.com>
2020-03-30 19:04:56 -0400

    'On-Hover Functionality'

--- connect4.js
+++ connect4.js
@@ -3,8 +3,8 @@ class Connect4{
         this.ROWS = 6;
         this.COLS = 7;
         this.selector = selector;
-
         this.createGrid();
+        this.setupEventListeners();
     }
 
     createGrid(){
@@ -15,12 +15,39 @@ class Connect4{
             .addClass('row');
             for(let col = 0; col < this.COLS; col++){
                 const $col = $('<div>')
-                .addClass('col empty');
+                .addClass('col empty')
+                .attr('data-col', col)
+                .attr('data-row', row)
                 $row.append($col);
             }
             $board.append($row);
         }
-        
-        console.log($board.html());
     }
-}
+    
+    setupEventListeners(){
+        const $board = $(this.selector);
+
+        function findLastEmptyCell(col){
+            const cells = $(`.col[data-col='${col}']`)
+            for(let i = cells.length -1; i>= 0; i--){
+                const $cell = $(cells[i]);
+                if($cell.hasClass('empty')){
+                    return $cell;
+                }
+            }
+            return null
+        }
+
+        $board.on('mouseenter', '.col.empty', function(){
+            console.log('here', this);
+            const col = $(this).data('col');
+            const $lastEmptyCell = findLastEmptyCell(col);
+            $lastEmptyCell.addClass('next-red');
+            console.log(col);
+        })
+
+        $board.on('mouseleave', '.col', function(){
+            $('.col').removeClass('next-red');
+        })
+    }
+} 
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     // console.log('whats good');
-
     const connect4 = new Connect4('#connect4')
 
 }); 
