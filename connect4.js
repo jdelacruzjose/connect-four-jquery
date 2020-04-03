bd41cc1848d725c514c5176737ecad59444aa51e
Jose De la Cruz <jdelacruzjose@gmail.com>
2020-04-02 21:29:04 -0400

    'CheckWinner  Functionality'

--- connect4.js
+++ connect4.js
@@ -4,12 +4,17 @@ class Connect4{
         this.COLS = 7;
         this.player = 'red';
         this.selector = selector;
+        this.isGameOver = false;
+        this.onPlayerMove = function(){};
         this.createGrid();
         this.setupEventListeners();
     }
 
     createGrid(){
         const $board = $(this.selector);
+        $board.empty();
+        this.isGameOver = false;
+        this.player = 'red';
         
         //Nested For-Loop Function to create rows/columns
         //attr() jquery method adds a value to the selected element 
@@ -33,7 +38,7 @@ class Connect4{
 
         //Function to iterate the columns to select last cell
         function findLastEmptyCell(col){
-            const cells = $(`.col[data-col='${col}']`)
+            const cells = $(`.col[data-col='${col}']`);
             for(let i = cells.length -1; i>= 0; i--){
                 const $cell = $(cells[i]);
                 if($cell.hasClass('empty')){
@@ -45,6 +50,7 @@ class Connect4{
 
         //Hover CSS Styling function
         $board.on('mouseenter', '.col.empty', function(){
+            if(that.isGameOver) return;
             const col = $(this).data('col');
             const $lastEmptyCell = findLastEmptyCell(col);
             $lastEmptyCell.addClass(`next-${that.player}`);
@@ -57,46 +63,52 @@ class Connect4{
 
         //onClick CSS Styling Function
         $board.on('click', '.col.empty', function(){
+            if(that.isGameOver) return;
             const col = $(this).data('col');
-            const row = $(this).data('row');
+            // const row = $(this).data('row');
             const $lastEmptyCell = findLastEmptyCell(col);
             $lastEmptyCell.removeClass(`empty next-${that.player}`);
             $lastEmptyCell.addClass(that.player);
-            $lastEmptyCell.data('player', that.player)
+            $lastEmptyCell.data('player', that.player);
 
-            const winner = that.checkForWinner(row, col)
-            if(winner){
+            const winner = that.checkForWinner(
+                $lastEmptyCell.data('row'), 
+                $lastEmptyCell.data('col'))
+            if (winner){
+                that.isGameOver = true;
                 alert(`Game Over! Player ${that.player} has won!`);
+                $('.col.empty').removeClass('empty');
                 return;
             }
 
             that.player= (that.player === 'red') ? 'black' : 'red';
+            that.onPlayerMove();
             $(this).trigger('mouseenter');
             });
         }   
 
-        //CheckWinner Function
+        //CheckWinner Logic
         checkForWinner(row, col){
         const that = this; 
 
-        function $getCell(i, j){
-           return $(`.col[data-row ='${i}'][data-col ='${j}']`);
+        function $getCell(a, b){
+           return $(`.col[data-row ='${a}'][data-col ='${b}']`);
         } 
 
         function checkDirection(direction){
             let total = 0;
-            let i = row + direction.i;
-            let j = col + direction.j;
-            let $next = $getCell(i, j);
-            while(i >= 0 &&
-                i < that.ROWS &&
-                j >= 0 &&
-                j < that.COLS &&
+            let a = row + direction.a;
+            let b = col + direction.b;
+            let $next = $getCell(a, b);
+            while(a >= 0 &&
+                a < that.ROWS &&
+                b >= 0 &&
+                b < that.COLS &&
                 $next.data('player') === that.player){
                     total++;
-                    i += direction.i;
-                    j += direction.j;
-                    $next = $getCell(i, j);
+                    a += direction.a;
+                    b += direction.b;
+                    $next = $getCell(a, b);
                 }
             return total;    
         }
@@ -112,10 +124,27 @@ class Connect4{
              }
         }
 
-        function checkVerticals(){
-            return checkWin({i: -1, j: 0}, {i: 1, j:0});
+        function checkDiagonalBLtoTR(){
+            return checkWin({a: 1, b: -1}, {a: 1, b: 1})
         }
 
-        return checkVerticals();
-        }   
+        function checkDiagonalTLtoBR(){
+            return checkWin({a: 1, b: 1}, {a: 1, b: -1})
+        }
+
+        function checkVerticals(){
+            return checkWin({a: -1, b: 0}, {a: 1, b:0});
+        }
+
+        function checkHorizontals(){
+            return checkWin({a: 0, b: -1}, {a: 1, b:0});
+        }
+
+        return checkVerticals() || checkHorizontals() || checkDiagonalBLtoTR() || checkDiagonalTLtoBR();
+        }
+        
+        restart(){
+            this.createGrid();
+            this.onPlayerMove();
+        }
 } 
--- main.js
+++ main.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     // console.log('whats good');
     const connect4 = new Connect4('#connect4')
 
-}); 
+    connect4.onPlayerMove = function(){
+        $('#player').text(connect4.player)
+    }
+
+    $('#restart').click(function(){
+        connect4.restart();
+    });
+
+});
